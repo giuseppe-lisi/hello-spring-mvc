79fbcc149eb40550009a69890f258005336e33c5
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-17 12:12:24 +0200

    aggiunte pagine per sommare due numeri e per lanciare un dado con relative rotte nel controller AppController

---
+++ src/main/java/mvc/spring/esercizio/hello_spring_mvc/controller/AppController.java
@@ -0,0 +1,39 @@
+package mvc.spring.esercizio.hello_spring_mvc.controller;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/add")
+    public String addizionatore(Model model, @RequestParam int num1, int num2) {
+        int sum = num1 + num2;
+
+        model.addAttribute("sum", sum);
+
+        return "addizionatore";
+    }
+
+    @GetMapping("/dice")
+    public String dice(Model model) {
+        Random rand = new Random();
+        int num = rand.nextInt(1,7);
+
+        model.addAttribute("num", num);
+
+        return "dice";
+    }
+    
+}
